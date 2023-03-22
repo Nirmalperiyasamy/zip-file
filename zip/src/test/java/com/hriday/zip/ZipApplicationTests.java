532adcb2b8f71ed1a1b1937064b458f3671631c6
Nirmal <105903253+Nirmalperiasamy@users.noreply.github.com>
2023-03-22 15:46:44 +0530

    Initial commit

---
+++ zip/src/test/java/com/hriday/zip/ZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hriday.zip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
